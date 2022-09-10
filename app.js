77792a716b52139664b716a881a71943636915c4
lui5ib <estudiofibonacci@gmail.com>
2022-09-10 19:07:12 -0300

    Ejercicio 1

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const countries = require('./countries.json');
  */
 function getCountryByIso3(isoCode){
     //Complete with your code
+    const data_country = countries.filter(isoCode);
+    return console.log(data_country);
 }
 
 /**
